187e809f40ae697e0188a8eae78f353f2b515d62
Manu George <manu@trackplex.com>
2020-02-13 02:43:48 +0530

    Implemented routing with withrouter for higher order routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
 import './pages/homepage/homepage.styles.scss'
 import './App.css';
 
-function App() {
-  return <div> 
-    <Homepage/>
+
+const HatsPage = ()=>(
+  <div>
+    <h1>Hats Page</h1>
   </div>
-  
+)
+
+
+function App() {
+  return (<div>
+    <Switch>
+      <Route exact path ='/' component = {Homepage}/>
+      <Route  path ='/hats' component = {HatsPage}/>
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss';
 class Directory extends React.Component{
     constructor(){
         super();
@@ -10,7 +10,7 @@ class Directory extends React.Component{
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -45,8 +45,11 @@ class Directory extends React.Component{
     render(){
         return(
             <div className="directory-menu">
-                {this.state.sections.map(({title,imageUrl,id,size})=>(
+                {/* {this.state.sections.map(({title,imageUrl,id,size})=>(
                     <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                ))} */   }
+                 {this.state.sections.map(({id, ...otherSectionProps})=>(
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))}
             </div>
         )
