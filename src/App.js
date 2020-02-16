eb4cc50b372a00324b40b2219dc6ef68eea0c573
Manu George <manu@trackplex.com>
2020-02-16 18:47:35 +0530

    Added Sign In and component and added styles

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Homepage from './pages/homepage/homepage.component'
 import './pages/homepage/homepage.styles.scss'
 import ShopPage from './pages/shop/shop.component'
 import './App.css';
+import Header from './components/header/header.component'
+import signInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
-
-// const HatsPage = ()=>(
-//   <div>
-//     <h1>Hats Page</h1>
-//   </div>
-// )
 
 
 function App() {
   return (<div>
+    <Header/>
     <Switch>
       <Route exact path ='/' component = {Homepage}/>
       <Route  path ='/shop' component = {ShopPage}/>
+      <Route  path ='/signin' component = {signInAndSignUpPage}/>
     </Switch>
   </div>
   );
